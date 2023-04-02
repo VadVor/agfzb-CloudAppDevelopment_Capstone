1c30a34b6e76791616a47c7f1b6a134b9acbb0bd
Vad_Vor <vadimvoronov195@gmail.com>
2023-04-02 19:11:31 +0200

    add change

--- functions/sample/python/main.py
+++ functions/sample/python/main.py
@@ -10,20 +10,20 @@ from cloudant.error import CloudantException
 import requests
 
 def main():
-   current_path = os.getcwd()
-   print(current_path)
-   with open('/.creds-sample.json', 'r') as f:
-    config = json.load(f)
+    current_path = os.getcwd()
+    print(current_path)
+    with open('/.creds-sample.json', 'r') as file:
+    config = json.load(file)
     try:
-        client = Cloudant.iam(None,config['IAM_API_KEY'],
+            client = Cloudant.iam(None,config['IAM_API_KEY'],
             url=config["COUCH_URL"],connect=True,)
-        print(f"Databases: {client.all_dbs()}")
+            print(f"Databases: {client.all_dbs()}")
     except CloudantException as cloudant_exception:
-        print("unable to connect")
-        return {"error": cloudant_exception}
+            print("unable to connect")
+            return {"error": cloudant_exception}
     except (requests.exceptions.RequestException, ConnectionResetError) as err:
-        print("connection error")
-        return {"error": err}
+            print("connection error")
+            return {"error": err}
 
     return {"dbs": client.all_dbs()}
 

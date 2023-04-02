05c5c4e56491cac72c6391338e9f99abe6355083
Vad_Vor <vadimvoronov195@gmail.com>
2023-04-02 21:21:37 +0200

    add changes

--- functions/sample/python/get_review.py
+++ functions/sample/python/get_review.py
@@ -20,9 +20,9 @@ def main (dic):
     except CloudantException as cloudant_exception:
         print("unable to connect")
         return {"error": cloudant_exception}
-    except (requests.exceptions.RequestException, ConnectionResetError) as er:
+    except (requests.exceptions.RequestException, ConnectionResetError) as error:
         print("connection error")
-        return {"error": er}
+        return {"error": error}
 
 if __name__=='__main__':
     main({"dealerId": 15})

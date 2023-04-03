620ee1700033961a9d9ccd4d768ce219116869d3
Vad_Vor <vadimvoronov195@gmail.com>
2023-04-03 18:52:06 +0200

    add models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,6 +9,12 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=20)
+    description = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name + "," + self.description
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -19,7 +25,20 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    TYPES = [("SEDAN", "Sedan"), ("SUV", "SUV"), ("WAGON", "Wagon"), ("LIMOUSINE", "Limousine"), ("CABRIOLET", "Cabriolet")]
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(null=False, max_length=20)
+    dealerId = models.IntegerField()
+    car_type = models.CharField(null=False,max_length=20,choices=TYPES, default="SEDAN")
+    year = models.DateField(default=now)
 
+    def __str__(self):
+        return  "Name: " + self.name + \
+                " Make Name: "+ self.make.name + \
+                " Type: " + self.car_type + \
+                " Dealer ID: " + str(self.dealerId)+ \
+                " Year: " + str(self.year)
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
